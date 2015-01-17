31d19061684c912408b041f2007bda51b17faa79
lafras <lafras@xietel.com>
2015-01-17 05:16:34 +0200

    npm install tweaks.

--- run_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /root/sql-mvc
-mkdir /var/log/sql-mvc/ -p
-/usr/local/bin/forever -o /var/log/sql-mvc/out.log -e /var/log/sql-mvc/err.log --workingDir /root/sql-mvc/ start -c /usr/local/bin/node /root/sql-mvc/app.js -p 80
-
-
--- win-test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-
-console.log('Node win started ok');
