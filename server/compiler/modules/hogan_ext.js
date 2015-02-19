a88c08de7c877204b95f5950e5da286ca5af2749
lafras <lafras@xietel.com>
2015-02-19 10:32:18 +0200

    Added hogan_ext.js

---
+++ server/compiler/modules/hogan_ext.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var hogan = require("hogan");
+var marked = require('marked');
+
+exports.module_name = 'hogan_ext.js';
+
+marked.setOptions({
+	gfm : true,
+	tables : true,
+	breaks : true,
+	pedantic : false,
+	sanitize : false,
+	smartLists : true,
+	smartypants : false
+});
+
+exports.compile_render = function (zx, cx , Template) {
+
+    //escape text embeded
+    if (cx.Text) {
+        cx.Text = cx.Text.replace(/{{/g,'@@[@@[');		
+        cx.Text = cx.Text.replace(/}}/g,'@@]@@]');		
+    }
+    
+	cx.proper = function () {
+			return function (val) {
+				var template = hogan.compile(val);
+                //console.log('properproperproperproperproperproperproperproperproperproperproperproper:',cx);
+				var Result = template.render(cx);
+				var res = zx.Beautify(String(Result));
+				//console.log('properproperproperproperproperproperproperproperproperproperproperproper:',res);
+				return res;
+			};
+		}
+
+	cx.quotes = function () {
+			return function (val) {
+				var template = hogan.compile(val);                
+				var Result = template.render(cx);                
+                Result = Result.replace(/\n/g,' ');				
+                Result = Result.replace(/\'/g,'"');				
+				return "'"+Result+"'";
+			};
+		}
+        
+
+	cx.marked = function () {
+			return function (val) {
+				var template = hogan.compile(val);                
+				var Result = template.render(cx);
+                Result = marked(Result);
+                Result = Result.replace(/\n/g,'<br>');				
+                Result = Result.replace(/\'/g,'"');				
+				return Result;
+			};
+		}
+                
+        
+        
+	var code = hogan.compile(Template);
+	var Result = code.render(cx);
+    Result = Result.replace(/\@\@\[\@\@\[/g,'{{');		
+    Result = Result.replace(/\@\@\]\@\@\]/g,'}}');		
+    	
+
+    return Result;
+}
+
+    
+    
