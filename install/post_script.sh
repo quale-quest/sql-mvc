c4282a819a4c8f4d5e0c7963df90716560424c75
lafras <lafras@xietel.com>
2015-03-10 09:05:21 +0200

    npm install - auto install firebird and app

--- install/post_script.sh
+++ install/post_script.sh
@@ -2,3 +2,6 @@
 
 sudo apt-get install -y firebird2.5-classic firebird-dev
 sudo service firebird2.5-classic start
+sudo chmod o+r /etc/firebird/2.5/SYSDBA.password
+
+node server\compiler\compile.js app Home/Guest all
