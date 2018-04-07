92fe70107a8fd00b321a2366b16f84cb62f78ea5
Lafras <lafras@many-maker.com>
2018-04-07 04:54:12 +0200

    Split Z$RUN

--- server/compiler/update_ref.sh
+++ server/compiler/update_ref.sh
@@ -2,7 +2,6 @@
 #  
 #Copies the output as a reference for future comparisons
 
-cd ../..
 cp output/Home/Guest/Index.sql ../Index.sql
 cp output/Home/User/Index.sql ../UserIndex.sql
 cp output/Home/Guest/Dashboard/Dashboard.sql ../Dashboard.sql
